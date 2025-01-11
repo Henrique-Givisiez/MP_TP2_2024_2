19a417cd46a2110fd1c37b3e1bb29bed263a2a7a
Henrique-Givisiez <lucogds@gmail.com>
2025-01-11 16:18:17 -0300

    teste 1 corrigido

--- testa_conta_palavra.cpp
+++ testa_conta_palavra.cpp
@@ -5,5 +5,5 @@
 #include <stdio.h>
 
 TEST_CASE("Conta palavra", "[single-file]" ) {
-    REQUIRE(ContaPalavra("./arquivos teste/teste1.txt") == "Este,2,é,1,o,1,texto,2,que,1,será,1,utilizado,1");
+    REQUIRE(ContaPalavra("./arquivos teste/teste1.txt") == "é,1,Este,1,o,1,que,1,será,1,texto,2,utilizado,1");
 }
