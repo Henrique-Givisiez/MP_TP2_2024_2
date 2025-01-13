72a44afc04a8e1cf3252e4b438bbeb439374f177
Henrique-Givisiez <lucogds@gmail.com>
2025-01-13 10:09:07 -0300

    teste 2 - sucesso

--- testa_conta_palavra.cpp
+++ testa_conta_palavra.cpp
@@ -6,10 +6,12 @@
 
 TEST_CASE("Conta palavra", "[single-file]") {
     char* teste1 = ContaPalavra("./arquivos teste/teste1.txt");
-
-    std::string result(teste1);
+    std::string result1(teste1);
     free(teste1);
+    REQUIRE(result1 == "é,1,Este,1,o,1,que,1,será,1,texto,2,utilizado,1");
 
-    // Compare com std::string literal
-    REQUIRE(result == "é,1,Este,1,o,1,que,1,será,1,texto,2,utilizado,1");
+    char* teste2 = ContaPalavra("./arquivos teste/teste2.txt");
+    std::string result2(teste2);
+    free(teste2);
+    REQUIRE(result2 == "é,1,Este,1,o,1,que,1,será,1,texto,2,utilizado,1");
 }
